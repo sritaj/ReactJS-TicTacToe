e15fad14a351f400d3d8058e82f5321ae2cc2b6d
Sritaj <sritajp@gmail.com>
2021-05-14 00:10:17 +0530

    Added imports, Added array to hold the cell values, Added States for Cross and Win Message, Added 3 methods for reloading game, checkIsWinner, and change item

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Icon from "./components/Icon";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
-function App() {
+const itemArray = new Array(9).fill("empty");
+
+const App = () => {
+  const [isCross, setIsCross] = useState(false);
+  const [winMessage, setWinMessage] = useState("");
+
+  const reloadGame = () => {};
+  const checkIsWinner = () => {};
+  const changeItem = (itemNumber) => {};
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +34,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
