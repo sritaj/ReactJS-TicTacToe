9b6951de7f379c62810cc0802bc3a5663e522bc9
Sritaj <sritajp@gmail.com>
2021-05-14 01:19:58 +0530

    Added logic for checkWinner method

--- src/App.js
+++ src/App.js
@@ -19,7 +19,57 @@ const App = () => {
     setWinMessage("");
     itemArray.fill("empty", 0, 9);
   };
-  const checkIsWinner = () => {};
+  const checkIsWinner = () => {
+    if (
+      itemArray[0] === itemArray[1] &&
+      itemArray[1] === itemArray[2] &&
+      itemArray[0] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[0]} wins`);
+    } else if (
+      itemArray[3] === itemArray[4] &&
+      itemArray[3] === itemArray[5] &&
+      itemArray[3] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[3]} wins`);
+    } else if (
+      itemArray[6] === itemArray[7] &&
+      itemArray[6] === itemArray[8] &&
+      itemArray[6] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[6]} wins`);
+    } else if (
+      itemArray[0] === itemArray[3] &&
+      itemArray[3] === itemArray[6] &&
+      itemArray[0] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[0]} wins`);
+    } else if (
+      itemArray[1] === itemArray[4] &&
+      itemArray[4] === itemArray[7] &&
+      itemArray[1] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[1]} wins`);
+    } else if (
+      itemArray[3] === itemArray[6] &&
+      itemArray[6] === itemArray[8] &&
+      itemArray[3] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[3]} wins`);
+    } else if (
+      itemArray[0] === itemArray[4] &&
+      itemArray[4] === itemArray[8] &&
+      itemArray[0] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[0]} wins`);
+    } else if (
+      itemArray[2] === itemArray[4] &&
+      itemArray[4] === itemArray[6] &&
+      itemArray[2] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[2]} wins`);
+    }
+  };
   const changeItem = (itemNumber) => {
     if (winMessage) {
       return toast(winMessage, { type: "success" });
